37901690c9aed6b31c14b11d8492df91312d0ac0
Bongani Dugmore Qunta <bonganidugmorequnta@gmail.com>
2026-01-04 22:34:25 +0000

    fixed malformed tag

--- clarins/clari/clari/obj/Debug/net10.0/clari.AssemblyInfo.cs
+++ clarins/clari/clari/obj/Debug/net10.0/clari.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clari")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda44d8a1441463a1f046105e642dfb4504caaad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4be6555bef41bf3d064a86de0d239f232497c01")]
 [assembly: System.Reflection.AssemblyProductAttribute("clari")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clari")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
